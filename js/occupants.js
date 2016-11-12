04bb0978bd196c130e7db379b6fe8b747f5f11a4
jeffyactive <jeff@reelyactive.com>
2016-11-12 14:29:07 -0500

    Initial commit

---
+++ js/occupants.js
@@ -0,0 +1,17 @@
+/**
+ * occupants Module
+ * All of the JavaScript specific to the notman-occupants is contained inside
+ * this angular module.  The only external dependencies are:
+ * - AngularUI Bootstrap
+ * - cormorant (reelyActive)
+ */
+angular.module('occupants', [ 'ui.bootstrap', 'reelyactive.cormorant' ])
+
+
+/**
+ * InteractionCtrl Controller
+ * Handles the manipulation of all variables accessed by the HTML view.
+ */
+.controller('InteractionCtrl', function($scope, cormorant) {
+
+});
